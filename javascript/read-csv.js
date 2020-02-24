8af682f228f3ac7eb335d6fe6c76b4ab1c610a7c
Mathias Zechmeister <zechmeister@astro.physik.uni-goettingen.de>
2020-02-24 23:24:01 +0100

    html helper file.

---
+++ javascript/read-csv.js
@@ -0,0 +1,68 @@
+var lines;
+function handleFiles(files) {
+   // Check for the various File API support.
+   if (window.FileReader) {
+      // FileReader are supported.
+      getAsText(files[0]);
+   } else {
+      alert('FileReader are not supported in this browser.');
+   }
+}
+
+function handleURL(url) {
+   // Check for the various File API support.
+   console.log(url)
+   if (window.FileReader) {
+      // FileReader are supported.
+      getAsText(url);
+   } else {
+      alert('FileReader are not supported in this browser.');
+   }
+}
+
+function getAsText(fileToRead) {
+   var reader = new FileReader();
+   // Handle errors load
+   reader.onload = loadHandler;
+   reader.onerror = errorHandler;
+   // Read file into memory as UTF-8
+   reader.readAsText(fileToRead);
+}
+
+function loadHandler(event) {
+   var csv = event.target.result;
+   processData(csv);
+   parse();
+   plotdata(t,y);
+}
+
+function processData(csv) {
+    var allTextLines = csv.split(/\r\n|\n/);
+    lines = [];
+    while (allTextLines.length) {
+        lines.push(allTextLines.shift().split(','));
+    }
+   console.log(lines);
+   drawOutput(lines);
+}
+
+function errorHandler(evt) {
+   if(evt.target.error.name == "NotReadableError") {
+      alert("Cannot read file !");
+   }
+}
+
+function drawOutput(lines){
+   //Clear previous data
+   document.getElementById("output").innerHTML = "";
+   var table = document.createElement("table");
+   for (var i = 0; i < lines.length; i++) {
+      var row = table.insertRow(-1);
+      for (var j = 0; j < lines[i].length; j++) {
+         var firstNameCell = row.insertCell(-1);
+         firstNameCell.appendChild(document.createTextNode(lines[i][j]));
+      }
+   }
+   document.getElementById("output").appendChild(table);
+}
+
