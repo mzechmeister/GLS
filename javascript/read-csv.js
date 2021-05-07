ebcb18cd2cbdd69b5cec9e431da0a3b60a6a08f9
Mathias Zechmeister <zechmeister@astro.physik.uni-goettingen.de>
2021-05-07 11:50:36 +0200

    feat: Load by url. Fixes #17.
    It will work only for urls allowing CORS.

--- javascript/read-csv.js
+++ javascript/read-csv.js
@@ -1,21 +1,40 @@
 var lines;
 
+function getCSV(url, func) {
+   var rawFile = new XMLHttpRequest();
+ 
+   rawFile.open("GET", url, false);
+   rawFile.overrideMimeType("text/plain");  // otherwise in firefox XML Parsing Error: syntax error
+   rawFile.onreadystatechange = function () {
+      if (rawFile.readyState === 4)
+         if (rawFile.status === 200 || rawFile.status == 0)
+            lines = rawFile.responseText;
+            func();
+        };
+        rawFile.send();
+}
+
+function handleURL(url) {
+   // Check for the various File API support.
+   console.log("requesting", url)
+   getCSV(url, function(){
+      if (lines.length)  {
+         lines = lines.split(/\r\n|\n/)
+         //lines = lines.slice(1,-1);
+         parse();
+         plotdata(t,y);
+      } else {
+         document.getElementById("output").innerHTML = "<a href='"+url+"'>"+url+"</a><br> could not be loaded. Maybe a <a href='https://en.wikipedia.org/wiki/Cross-origin_resource_sharing'>CORS</a> issue. Try:<br>http://cdsarc.unistra.fr/ftp/J/A+A/552/A78/harps/hr3259_h.dat"
+// https://raw.githubusercontent.com/plotly/datasets/master/spectral.csv
+      }
+   })
+}
+
 function handleFiles(files) {
    // Check for the various File API support.
    if (window.FileReader) {
       // FileReader are supported.
       getAsText(files[0]);
-   } else {
-      alert('FileReader are not supported in this browser.');
-   }
-}
-
-function handleURL(url) {
-   // Check for the various File API support.
-   console.log(url)
-   if (window.FileReader) {
-      // FileReader are supported.
-      getAsText(url);
    } else {
       alert('FileReader are not supported in this browser.');
    }
